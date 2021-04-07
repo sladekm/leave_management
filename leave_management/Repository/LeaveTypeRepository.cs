86234cb6efc53fd1bc54178e278b980451d65803
Matyáš Sládek <acc.sladek@gmail.com>
2021-04-07 20:40:30 +0200

    Initial commit, database and repository pattern implemented

---
+++ leave_management/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using leave_management.Contracts;
+using leave_management.Data;
+
+namespace leave_management.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(LeaveType entity)
+        {
+            _db.LeaveTypes.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            _db.LeaveTypes.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+             return _db.LeaveTypes.ToList();
+        }
+
+        public LeaveType FindById(int id)
+        {
+            return _db.LeaveTypes.Find(id);
+        }
+
+        public ICollection<LeaveType> GetEmployeesByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            _db.LeaveTypes.Update(entity);
+            return Save();
+        }
+    }
+}
